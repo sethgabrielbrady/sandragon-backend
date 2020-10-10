758022ca0469f5dd9376ad1eea24d98322cda54a
Seth Brady <sethgabrielbrady@gmail.com>
2020-10-10 14:55:40 -0400

    Set up initial sanddragon backend

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      before_action :authorize_access_request!, except: [:show, :index]
+      before_action :set_post, only: [:show, :update, :destroy]
+
+      # GET /posts
+      def index
+        @posts = current_user.posts.all
+
+        render json: @posts
+      end
+
+      # GET /posts/1
+      def show
+        render json: @post
+      end
+
+      # POST /posts
+      def create
+        @post = current_user.posts.new(post_params)
+
+        if @post.save
+          render json: @post, status: :created, location: @post
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /posts/1
+      def update
+        if @post.update(post_params)
+          render json: @post
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /posts/1
+      def destroy
+        @post.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_post
+          @post = current_user.posts.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
+        def post_params
+          params.require(:post).permit(:title, :content, :user_id)
+        end
+
+    end
+  end
+end
